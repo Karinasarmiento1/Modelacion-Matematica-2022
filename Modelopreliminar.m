f570a8a2b82fce794ab5a28a9840daf4871e36f8
Yesy Karina Sarmiento Perdomo <karinasarmiento20@gmail.com>
2022-11-26 18:50:43 -0600

    Karina Sarmiento

---
+++ Modelopreliminar.m
@@ -0,0 +1,41 @@
+function [X,Y,A,Aid] = Modelopreliminar(N)
+
+x0 = [1 0 0 0].';
+X = x0;
+A = [0.95,0.04,0,0;
+     0.05,0.85,0,0;
+     0,0.10,1,0;
+     0,0.01,0,1];
+
+for k=1:N
+	X = [X A*X(:,k)];
+end
+
+Y = X(:,2:(N+1));
+X = X(:,1:N);
+[U,D,V ] =svd(X);
+D1=pinv(D);
+XI=V*D1*U';
+Aid = Y*XI;
+
+disp(['||Aid X-Y||_F = ',num2str(norm(Aid*X-Y,'fro'))])
+disp(['||Aid-A||_F = ',num2str(norm(Aid-A,'fro'))])
+
+X1 = X0 = x0;
+
+for k = 1:300
+	X0 = [X0 A*X0(:,k)];
+	X1 = [X1 Aid*X1(:,k)];
+end
+
+subplot(211);
+plot(X0.');
+title('Órbitas del modelo original')
+
+subplot(212);
+plot(X1.');
+title('Órbitas del modelo identificado')
+
+disp(['Estimación de diferencias entre órbitas de prueba modelos ||X_0^tst-X_1^tst||_1 = ',num2str(norm(X0(:,7:end)-X1(:,7:end),1))])
+
+end
